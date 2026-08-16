c6660ba973762e03dad29c52b28b9d63720a90be
Diogo Belo <diogobelodss22@gmail.com>
2026-08-16 15:15:23 -0300

    fix bug in page tsx

--- winfixer/app/page.tsx
+++ winfixer/app/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import React, { useState } from 'react';
-import Header from '../components/Header';
 import ErrorSearch from '../components/ErrorSearch';
 
 const HomePage = () => {
@@ -15,7 +14,7 @@ const HomePage = () => {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <Header />
+      
       <main className="flex flex-col items-center w-full max-w-2xl p-4">
         <h1 className="text-4xl font-bold text-center text-gray-800">WinFixer</h1>
         <p className="mt-2 text-lg text-center text-gray-600">
